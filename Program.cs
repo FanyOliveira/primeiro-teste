945d6cfd43168ca8996b6fdc1c5035b63d8f6c2d
FanyOliveira <135743057+FanyOliveira@users.noreply.github.com>
2023-06-06 16:24:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uma mensagem de testes");
+            Console.WriteLine("Estou enviando para o GitHub");
+            Console.ReadKey();
+        }
+    }
+}
